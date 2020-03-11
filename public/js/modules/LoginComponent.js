676acf49961af415fe9494cb833851341cb8c243
ling89889988952 <zouling707@gmail.com>
2020-03-11 12:03:06 -0400

    modify

---
+++ public/js/modules/LoginComponent.js
@@ -0,0 +1,56 @@
+export default {
+   
+    template: `
+    <div id="formapp">
+        <h2>Welcome to ChatApp!</h2>
+        <p>Please fill in the form below.</p>
+        <form @submit.prevent="login" id="my-form">
+            <label for="username">Username:</label>
+            <input v-model="input.username" type="text" name="username" id="username" value="">
+
+            <label for="password">Password:</label>
+            <input v-model="input.password" id="password" type="password" name="password"   value="">
+
+            <label for="nickname">Nickname</label>
+            <input v-model="input.nickname" id="nickname" type="text" name="nickname"   value="">
+
+            <input type="submit" value="Login">
+          
+        </form>
+    </div> 
+    `,
+
+    data(){
+        return {
+            input:{
+                username: '',
+                password: '',
+                nickname:''
+            },
+            
+        }
+    },
+
+    methods:{
+        login() {
+            if(this.input.username != '' && this.input.password != ''){
+                if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
+                    this.$emit("authenticated", true);
+                    this.$emit("notice", true);
+
+                    // this.$emit("nickname", this.input.nickname);
+                    this.$router.replace({ name: "message" });
+                } else {
+                    console.log("The username and / or password is incorrect");
+                }
+            } else {
+                console.log("A username password and nickname must be present");
+            }
+
+            }
+        },
+
+       
+    }
+
+    
---
+++ public/js/modules/MessageComponent.js
@@ -0,0 +1,103 @@
+// the export statement means that everything inside the curly braces 
+// will be made public when you import this file into another via the import statement
+import ChatComponent from "./ChatComponent.js";
+
+const socket = io();
+
+export default {
+    template: `
+    <div>
+        <section class="form-container">
+			<label for="nickname">Your Nickname</label>
+			<input v-model="nickname" type="text" id="name" class="name" placeholder="What is your nickname?">
+
+			<form>
+				<label for="message">Something to say?</label>
+				<textarea v-model="message" class="message" type="text" autocomplete="off" id="textarea"></textarea>
+				<input @click.prevent="dispatchMessage" type="submit">
+			</form>
+		</section>
+
+        <section class="messages">
+			<ul id="messages">
+				<!-- render a new message component for every message -->
+                <newmessage v-for="message in messages" :msg="message">
+                </newmessage>
+			</ul>
+        </section>
+
+        </div>
+        `
+
+    ,
+
+    
+
+    data(){
+        return {
+                nickname: '',
+                message:'',
+                messages: [],
+                socketID : this.$parent.sockData.socketID,
+                // matchedID: this.$parent.sockData.socketID == this.msg.id,
+                // content: this.message    
+
+                // message:{
+                //     nickname: this.$parent.sockData.nickname,
+                //     content: this.$parent.sockData.message,
+                //     matchedID: this.$parent.sockData.socketID 
+                // }
+        }
+     
+    },
+
+    created: function(){
+        this.appendMessage() 
+    
+    },
+   
+    methods: {
+        
+        // emit a message event to the server so that it can in 
+        // turn send this to anyone who's connected
+        dispatchMessage() {
+            console.log('handle emit message');
+                //  this.input.nickname == this.$parent.sockData.nickname
+                //  this.input.message  == this.$parent.sockData.message
+                    // this.message =this.$parent.sockData.message
+                    // this.nickname =this.$parent.sockData.nickname
+                    socket.emit('chat_message', {
+                        content:  this.message,
+                        name: this.nickname 
+                        // || "anonymous"
+                    })
+        
+                    this.message = "";
+                },
+
+        appendMessage(){
+            
+            messages.push(message)
+
+            }
+        },
+        
+
+    mouted: function() {
+        console.log('vue is done mounting');
+    },
+
+    components: {
+        newmessage: ChatComponent
+    }  
+}
+
+
+
+
+
+
+
+
+
+
